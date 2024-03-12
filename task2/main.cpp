bdb07d10415f466cd1a019470e8d3cf93cdf7123
root <root@DESKTOP-82M5BL7.localdomain>
2024-03-12 21:13:12 +0300

    to think

--- task2/main.cpp
+++ task2/main.cpp
@@ -7,26 +7,26 @@ using namespace std;
 
 namespace constans {
 const int STEPS = 1024;
-const double LEFT_BORDER = 0;
-const double EPSILON = 1e-6;
-const double E = 2.71828182846;
+const float LEFT_BORDER = 0;
+const float EPSILON = 1e-6;
+const float E = 2.71828182846;
 }  // namespace constans
 
-double McLorenFunc(double x, int steps) {
-  double result = 0;
+float McLorenFunc(float x, int steps) {
+  float result = 0;
   for (int n = 0; n < steps; ++n) {
     result += pow(-1, n) * pow(x, 2 * n + 1) / (tgamma(n + 1) * (2 * n + 1));
   }
   return 2 / sqrt(M_PI) * result;
 }
 
-double func(double t) { return (2 / sqrt(M_PI)) * pow(constans::E, -(t * t)); }
+float func(float t) { return (2 / sqrt(M_PI)) * pow(constans::E, -(t * t)); }
 
-double leftRectangles(double (*func)(double), const double &a, double b,
+float leftRectangles(float (*func)(float), const float &a, float b,
                       int steps) {
-  double result = 0.0;
-  double h = (b - a) / steps;
-  double x_i = 0.0;
+  float result = 0.0;
+  float h = (b - a) / steps;
+  float x_i = 0.0;
   for (int i = 0; i < steps; i++) {
     x_i = a + h * i;
     result += h * func(x_i);
@@ -34,11 +34,11 @@ double leftRectangles(double (*func)(double), const double &a, double b,
   return result;
 }
 
-double rightRectangles(double (*func)(double), const double &a, double b,
+float rightRectangles(float (*func)(float), const float &a, float b,
                        int steps) {
-  double result = 0.0;
-  double h = (b - a) / steps;
-  double x_i_1 = 0.0;
+  float result = 0.0;
+  float h = (b - a) / steps;
+  float x_i_1 = 0.0;
   for (int i = 1; i <= steps; i++) {
     x_i_1 = a + h * i;
     result += h * func(x_i_1);
@@ -46,12 +46,12 @@ double rightRectangles(double (*func)(double), const double &a, double b,
   return result;
 }
 
-double middleRectangles(double (*func)(double), const double &a, double b,
+float middleRectangles(float (*func)(float), const float &a, float b,
                         int steps) {
-  double result = 0.0;
-  double h = (b - a) / steps;
-  double x_i = 0.0;
-  double x_i_1 = 0.0;
+  float result = 0.0;
+  float h = (b - a) / steps;
+  float x_i = 0.0;
+  float x_i_1 = 0.0;
   for (int i = 1; i <= steps; i++) {
     x_i = a + h * (i - 1);
     x_i_1 = a + h * i;
@@ -60,11 +60,11 @@ double middleRectangles(double (*func)(double), const double &a, double b,
   return result;
 }
 
-double trapezeFormula(double (*func)(double), const double &a, double b,
+float trapezeFormula(float (*func)(float), const float &a, float b,
                       int steps) {
-  double result = func(a) + func(b);
-  double h = (b - a) / steps;
-  double x_i_1 = 0.0;
+  float result = func(a) + func(b);
+  float h = (b - a) / steps;
+  float x_i_1 = 0.0;
   for (int i = 1; i <= steps; i++) {
     x_i_1 = a + h * i;
     result += 2 * func(x_i_1);
@@ -73,11 +73,11 @@ double trapezeFormula(double (*func)(double), const double &a, double b,
   return result;
 }
 
-double SypmsonsFormula(double (*func)(double), const double &a, double b,
+float SypmsonsFormula(float (*func)(float), const float &a, float b,
                        int steps) {
-  double h = (b - a) / steps;
-    double result = 0;
-    double x = 0;
+  float h = (b - a) / steps;
+    float result = 0;
+    float x = 0;
     for (int i = 0; i < steps; i++)
     {
         result += (func(x) + 4 * func(x + h / 2) + func(x + h)) * h / 6;
@@ -86,13 +86,13 @@ double SypmsonsFormula(double (*func)(double), const double &a, double b,
     return result;
 }
 
-double GaussFormula(double (*func)(double), const double &a, double b,
+float GaussFormula(float (*func)(float), const float &a, float b,
                        int steps) {
-    double h = (b - a) / steps;
-    double ad1 = (1 - 1.0 / sqrt(3)) * h / 2;
-    double ad2 = (1 + 1.0 / sqrt(3)) * h / 2;
-    double result = 0;
-    double x = 0;
+    float h = (b - a) / steps;
+    float ad1 = (1 - 1.0 / sqrt(3)) * h / 2;
+    float ad2 = (1 + 1.0 / sqrt(3)) * h / 2;
+    float result = 0;
+    float x = 0;
     for (int i = 0; i < steps; i++)
     {
         result += (func(x + ad1) + func(x + ad2)) * h / 2;
@@ -101,31 +101,31 @@ double GaussFormula(double (*func)(double), const double &a, double b,
     return result;
 }
 
-void CalculateFunc(vector<double> points,
-                   double (*function)(double (*func)(double), const double &,
-                                      double, int)) {
+void CalculateFunc(vector<float> points,
+                   float (*function)(float (*func)(float), const float &,
+                                      float, int)) {
   for (auto point : points) {
     int i = 1;
-    double last_j = 0.0;
-    double j = 0.0;
+    float last_j = 0.0;
+    float j = 0.0;
     do {
-      i++;
+      i *= 2;
       last_j = j;
       j = function(func, constans::LEFT_BORDER, point, i);
-    } while (fabs(last_j - j) > constans::EPSILON && i <= constans::STEPS);
+    } while (abs(last_j - j) > constans::EPSILON && i < constans::STEPS);
 
-    double difference = fabs(McLorenFunc(point, 15) - j);
+    float difference = abs(McLorenFunc(point, 12) - j);
 
     printf(
         "x_i = %.1lf | J_o = %.6lf | J_n = %.6lf | |J_o - J_n| = %.6lf | N "
         "= %d\n",
-        point, McLorenFunc(point, 15), j, difference, i - 1);
+        point, McLorenFunc(point, 12), j, difference, i);
   }
 }
 
 
 int main() {
-  vector<double> points = {0.0, 0.2, 0.4, 0.6, 0.8, 1.0,
+  vector<float> points = {0.0, 0.2, 0.4, 0.6, 0.8, 1.0,
                            1.2, 1.4, 1.6, 1.8, 2.0};
   cout << "Правые прямоугольники\n";
   CalculateFunc(points, rightRectangles);
