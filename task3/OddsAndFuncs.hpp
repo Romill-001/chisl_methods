f8d06f0d7c06a99def009aadc5159b53dc294c5c
root <root@DESKTOP-82M5BL7.localdomain>
2024-04-17 01:02:44 +0300

    first

---
+++ task3/OddsAndFuncs.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+namespace odds {
+    int n = 10;
+    std::vector<double> alpha(n);
+    std::vector<double> betta(n);
+    std::vector<double> a(n);
+    std::vector<double> b(n);
+    std::vector<double> c(n);
+    std::vector<double> y_i(n);
+    std::vector<double> a_i(n);
+    std::vector<double> q_i(n);
+    std::vector<double> u_i(n);
+    std::vector<double> f_i(n);
+}
+
+double f_x(double x) {
+    return -(std::pow(x,5)) + 22 * std::pow(x,4) - 36 * std::pow(x,3) + 28 * x * x - 19 * x + 6;
+}
+
+double u_x(double x) {
+    return x * std::pow((1 - x), 3);
+}
+
+double p_x(double x) {
+    return 1 + x*x;
+}
+
+double q_x(double x) {
+    return 1 + x;
+}
--- task3/main.cpp
+++ task3/main.cpp
@@ -1,8 +1,41 @@
-#include <algorithm>
-#include <cmath>
-#include <iostream>
-#include <vector>
+#include "OddsAndFuncs.hpp"
+
+using namespace std;
+using namespace odds;
 
 int main() {
+
+    double h = 1.0 / n;
+    alpha[1] = 0;
+    betta[1] = 0;
+    y_i[0] = 0;
+    y_i[0] = 0;
+    y_i[n] = 0;
+
+    for (int i = 0; i <= n; i++) {
+        a_i[i] = p_x(i * h);
+        q_i[i] = q_x(i * h);
+        u_i[i] = u_x(i * h);
+        f_i[i] = f_x(i * h) * pow(h, 2);
+    }
+
+    for (int i = 1; i < n; i++) {
+        a[i] = -a_i[i];
+        b[i] = a_i[i] + a[i + 1] + h * h * q_i[i];
+        c[i] = -a_i[i + 1];
+    }
+
+    for (int i = 1; i < n; i++) {
+        alpha[i + 1] = b[i] / (c[i] - alpha[i] * a[i]);
+        betta[i + 1] = (f_i[i] + a[i] * betta[i]) / (c[i] - alpha[i] * a[i]);
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        y_i[i] = alpha[i + 1] * y_i[i + 1] + betta[i + 1];
+    }
+
+    for (int i = 0; i <= n; i++) {
+        printf("ih = %4.1lf | y_i = %9.6lf | u(ih) = %8.6lf | |y_i - u(ih)| = %8.6lf\n", i * h, y_i[i], u_i[i], fabs(y_i[i]- u_i[i]));
+    }
     return 0;
 }
